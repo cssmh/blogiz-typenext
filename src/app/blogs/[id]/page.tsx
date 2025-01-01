1755bbb7f737a91679fe24f467f7e998138f7dfe
Momen <touristmomen@gmail.com>
2025-01-01 20:36:06 +0600

    details page added

---
+++ src/app/blogs/[id]/page.tsx
@@ -0,0 +1,18 @@
+import BlogDetails from "@/components/ui/BlogDetails";
+
+interface Id {
+  params: {
+    id: string;
+  };
+}
+const page = async ({ params }: Id) => {
+  const res = await fetch(`http://localhost:5000/blogs/${params.id}`);
+  const blog = await res.json();
+  return (
+    <div>
+      <BlogDetails blog={blog} />
+    </div>
+  );
+};
+
+export default page;
