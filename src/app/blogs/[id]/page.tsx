f9f3a8432a97e616a94b2f6c01bb2dba24733c67
Momen <touristmomen@gmail.com>
2025-01-02 10:05:36 +0600

    generateStaticParams

--- src/app/blogs/[id]/page.tsx
+++ src/app/blogs/[id]/page.tsx
@@ -1,10 +1,20 @@
 import BlogDetails from "@/components/ui/BlogDetails";
+import { Blog } from "@/types";
 
 interface Id {
   params: {
     id: string;
   };
 }
+
+export const generateStaticParams = async () => {
+  const res = await fetch("http://localhost:5000/blogs");
+  const blogs = await res.json();
+  return blogs.slice(0, 3).map((blog: Blog) => ({
+    id: blog.id,
+  }));
+};
+
 const page = async ({ params }: Id) => {
   const res = await fetch(`http://localhost:5000/blogs/${params.id}`, {
     cache: "no-store",
